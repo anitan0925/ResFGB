a6794476a09a7a94eef22c8d31a640c5c4943231
anitan <anitan0925@gmail.com>
2018-02-24 10:49:46 +0900

    Fixed experimental setup

--- examples/experiment.py
+++ examples/experiment.py
@@ -38,13 +38,13 @@ if dataset == 'letter':
     hparams['model_hparams']['wr']            = 1e-6
     hparams['model_hparams']['max_epoch']     = 100
     hparams['model_hparams']['early_stop']    = -1
-    hparams['model_hparams']['scale']         = 0.5
+    hparams['model_hparams']['scale']         = 1.0
     hparams['model_hparams']['eval_iters']    = 10000
     hparams['resblock_hparams']['shape']      = (input_dim,1000,1000,1000,input_dim)
     hparams['resblock_hparams']['wr']         = 1e-6
     hparams['resblock_hparams']['max_epoch']  = 100
     hparams['resblock_hparams']['early_stop'] = 10
-    hparams['resblock_hparams']['scale']      = 0.5
+    hparams['resblock_hparams']['scale']      = 1.0
     hparams['resblock_hparams']['eval_iters'] = 10000 
     hparams['fg_eta']    = 1e-2
     hparams['max_iters'] = 21
@@ -53,13 +53,13 @@ elif dataset == 'usps':
     hparams['model_hparams']['wr']            = 1e-4
     hparams['model_hparams']['max_epoch']     = 200
     hparams['model_hparams']['early_stop']    = -1
-    hparams['model_hparams']['scale']         = 0.5
+    hparams['model_hparams']['scale']         = 1.0
     hparams['model_hparams']['eval_iters']    = 10000
     hparams['resblock_hparams']['shape']      = (input_dim,1000,1000,1000,input_dim)
     hparams['resblock_hparams']['wr']         = 1e-4
     hparams['resblock_hparams']['max_epoch']  = 100
     hparams['resblock_hparams']['early_stop'] = 10
-    hparams['resblock_hparams']['scale']      = 0.5
+    hparams['resblock_hparams']['scale']      = 1.0
     hparams['resblock_hparams']['eval_iters'] = 10000
     hparams['fg_eta']    = 1e+0
     hparams['max_iters'] = 1
@@ -68,13 +68,13 @@ elif dataset == 'ijcnn1':
     hparams['model_hparams']['wr']            = 1e-5
     hparams['model_hparams']['max_epoch']     = 50
     hparams['model_hparams']['early_stop']    = -1
-    hparams['model_hparams']['scale']         = 1.
+    hparams['model_hparams']['scale']         = 1.0
     hparams['model_hparams']['eval_iters']    = 10000
     hparams['resblock_hparams']['shape']      = (input_dim,100,100,100,100,input_dim)
     hparams['resblock_hparams']['wr']         = 1e-5
     hparams['resblock_hparams']['max_epoch']  = 20
     hparams['resblock_hparams']['early_stop'] = 10
-    hparams['resblock_hparams']['scale']      = 1.
+    hparams['resblock_hparams']['scale']      = 1.0
     hparams['resblock_hparams']['eval_iters'] = 10000
     hparams['fg_eta']    = 1e-1
     hparams['max_iters'] = 28
@@ -83,13 +83,13 @@ elif dataset == 'mnist':
     hparams['model_hparams']['wr']            = 1e-6
     hparams['model_hparams']['max_epoch']     = 20
     hparams['model_hparams']['early_stop']    = -1
-    hparams['model_hparams']['scale']         = 1.
+    hparams['model_hparams']['scale']         = 1.0
     hparams['model_hparams']['eval_iters']    = 10000
     hparams['resblock_hparams']['shape']      = (input_dim,1000,1000,1000,input_dim)
     hparams['resblock_hparams']['wr']         = 1e-6
     hparams['resblock_hparams']['max_epoch']  = 20
     hparams['resblock_hparams']['early_stop'] = 10
-    hparams['resblock_hparams']['scale']      = 1.
+    hparams['resblock_hparams']['scale']      = 1.0
     hparams['resblock_hparams']['eval_iters'] = 10000
     hparams['fg_eta']    = 1e-1
     hparams['max_iters'] = 8
@@ -98,16 +98,18 @@ elif dataset == 'covtype':
     hparams['model_hparams']['wr']            = 1e-6
     hparams['model_hparams']['max_epoch']     = 10
     hparams['model_hparams']['early_stop']    = -1
-    hparams['model_hparams']['scale']         = 0.5
+    hparams['model_hparams']['scale']         = 1.0
     hparams['model_hparams']['eval_iters']    = 10000
     hparams['resblock_hparams']['shape']      = (input_dim,1000,1000,1000,input_dim)
     hparams['resblock_hparams']['wr']         = 1e-6
     hparams['resblock_hparams']['max_epoch']  = 20
     hparams['resblock_hparams']['early_stop'] = 10
-    hparams['resblock_hparams']['scale']      = 0.5
+    hparams['resblock_hparams']['scale']      = 1.0
     hparams['resblock_hparams']['eval_iters'] = 10000
     hparams['fg_eta']    = 1e-2
     hparams['max_iters'] = 50
+
+hparams['seed'] = seed
 
 model = ResFGB( **hparams )
 model.fit( X, Y )
